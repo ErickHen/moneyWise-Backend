77fcd38fbe681b8d8497c27ad0868c61a90d3bff
Erik Henrique <erickhenrique110410@gmail.com>
2023-05-07 20:07:26 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const gastosRoutes = require('./routes/gastosRoutes')
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD =  process.env.DB_PASSWORD
+
 require('dotenv').config();
 
 app.use(
@@ -11,16 +15,11 @@ app.use(
 
 app.use(express.json())
 
-const gastosRoutes = require('./routes/gastosRoutes')
-
 app.use('/gasto', gastosRoutes)
 
 app.get('/', (req, res) => {
     res.json({ message: 'Funcionando!' })
 })
-
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD =  process.env.DB_PASSWORD
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.kkjwoby.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
