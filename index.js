60eeb10186cd4fc4af7b921e21511a31452725e9
Erik Henrique <erickhenrique110410@gmail.com>
2023-05-07 18:50:57 -0300

    new commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const DB_PASSWORD =  process.env.DB_PASSWORD
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.kkjwoby.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
-        console.log(("Conectado ao mongoDB!"));
+        console.log("Conectado ao mongoDB!");
         app.listen(3000)
     })
     .catch((err) => console.log(err))
